842810cf53d7d6140e3da29937f450438b5afd3b
Thor Denson <tjdenson@eckerd.edu>
2018-07-24 14:07:08 -0400

    adds html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <header>
+          <h1> React Notes App </h1>
+          <input type="text" />
+        </header>
 
+        <div className="document-list">
+          <ul>
+            <li className="document-list-item">Note number one</li>
+            <li className="document-list-item">Note number two</li>
+            <li className="document-list-item">Note number three</li>
+          </ul>
+        </div>
+
+        <div className="document-editor">
+          <textarea value="This is a note."/>
+        </div>
       </div>
     );
   }
